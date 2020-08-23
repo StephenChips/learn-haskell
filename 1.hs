248686a4ba41de090434d02090d61692d047310e
StephenChips <tcw6991@gmail.com>
2020-08-23 21:05:44 +0800

    save

--- 1.hs
+++ 1.hs
@@ -3,3 +3,11 @@ cmp a
     | a > 1    = GT  
     | a == 1   = EQ  
     | otherwise = LT 
+
+
+search :: (Eq a) => a -> [a] -> Integer
+search target ls = search' 0 ls where
+    search' count [] = (-1)
+    search' count (i:xs)
+        | i == target = count
+        | otherwise = search' (count + 1) xs
